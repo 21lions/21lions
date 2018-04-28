00fc65a77c257653c3cb88f1d4e02f813a515553
21lions <vicgarza21@gmail.com>
2018-04-28 17:06:03 -0500

    Update and rename ImagesG.js to ImagesH.js

--- ImagesH.js
+++ ImagesH.js
@@ -33,29 +33,29 @@ $('h1.cfm-player-name:contains("Andrew Luck")').append
 $('h1.cfm-player-name:contains("Dak Prescott")').append
  ('<span class="playerbox"><img src="http://www.daddyleagues.com/img/m18/players/large/8179.png" class="playerimage"></span>');
 $('h1.cfm-player-name:contains("Ryan Tannehill")').append
- ('<span class="playerbox"><img src="http://www.daddyleagues.com/img/nfl/players/large/5504.png" class="playerimage"></span>');
+ ('<span class="playerbox"><img src="http://www.daddyleagues.com/img/m18/players/large/5504.png" class="playerimage"></span>');
 $('h1.cfm-player-name:contains("Patrick Mahomes II")').append
- ('<span class="playerbox"><img src="http://www.daddyleagues.com/img/nfl/players/large/8648.png" class="playerimage"></span>');
+ ('<span class="playerbox"><img src="http://www.daddyleagues.com/img/m18/players/large/8648.png" class="playerimage"></span>');
 $('h1.cfm-player-name:contains("Derek Carr")').append
- ('<span class="playerbox"><img src="http://www.daddyleagues.com/img/nfl/players/large/6374.png" class="playerimage"></span>');
+ ('<span class="playerbox"><img src="http://www.daddyleagues.com/img/m18/players/large/6374.png" class="playerimage"></span>');
 $('h1.cfm-player-name:contains("Braxton Miller")').append
- ('<span class="playerbox"><img src="http://www.daddyleagues.com/img/nfl/players/large/8147.png" class="playerimage"></span>');
+ ('<span class="playerbox"><img src="http://www.daddyleagues.com/img/m18/players/large/8147.png" class="playerimage"></span>');
 $('h1.cfm-player-name:contains("Aaron Rodgers")').append
- ('<span class="playerbox"><img src="http://www.daddyleagues.com/img/nfl/players/large/6299.png" class="playerimage"></span>');
+ ('<span class="playerbox"><img src="http://www.daddyleagues.com/img/m18/players/large/6299.png" class="playerimage"></span>');
 $('h1.cfm-player-name:contains("Matthew Stafford")').append
- ('<span class="playerbox"><img src="http://www.daddyleagues.com/img/nfl/players/large/6302.png" class="playerimage"></span>');
+ ('<span class="playerbox"><img src="http://www.daddyleagues.com/img/m18/players/large/6302.png" class="playerimage"></span>');
 $('h1.cfm-player-name:contains("Cam Newton")').append
- ('<span class="playerbox"><img src="http://www.daddyleagues.com/img/nfl/players/large/4439.png" class="playerimage"></span>');
+ ('<span class="playerbox"><img src="http://www.daddyleagues.com/img/m18/players/large/4439.png" class="playerimage"></span>');
 $('h1.cfm-player-name:contains("Jimmy Garoppolo")').append
- ('<span class="playerbox"><img src="http://www.daddyleagues.com/img/nfl/players/large/6429.png" class="playerimage"></span>');
+ ('<span class="playerbox"><img src="http://www.daddyleagues.com/img/m18/players/large/6429.png" class="playerimage"></span>');
 $('h1.cfm-player-name:contains("C.J. Beathard")').append
- ('<span class="playerbox"><img src="http://www.daddyleagues.com/img/nfl/players/large/8477.png" class="playerimage"></span>');
+ ('<span class="playerbox"><img src="http://www.daddyleagues.com/img/m18/players/large/8477.png" class="playerimage"></span>');
 $('h1.cfm-player-name:contains("Jared Goff")').append
- ('<span class="playerbox"><img src="http://www.daddyleagues.com/img/nfl/players/large/8021.png" class="playerimage"></span>');
+ ('<span class="playerbox"><img src="http://www.daddyleagues.com/img/m18/players/large/8021.png" class="playerimage"></span>');
 $('h1.cfm-player-name:contains("Russell Wilson")').append
- ('<span class="playerbox"><img src="http://www.daddyleagues.com/img/nfl/players/large/5566.png" class="playerimage"></span>');
+ ('<span class="playerbox"><img src="http://www.daddyleagues.com/img/m18/players/large/5566.png" class="playerimage"></span>');
 $('h1.cfm-player-name:contains("Matt Ryan")').append
- ('<span class="playerbox"><img src="http://www.daddyleagues.com/img/nfl/players/large/6301.png" class="playerimage"></span>');
+ ('<span class="playerbox"><img src="http://www.daddyleagues.com/img/m18/players/large/6301.png" class="playerimage"></span>');
 
 
 /*created quarterbacks*/
