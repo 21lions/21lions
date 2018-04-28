681d8be2da280edb2424d69d1d75555ad5162365
21lions <vicgarza21@gmail.com>
2018-04-28 14:43:56 -0500

    Update and rename ImagesA.js to ImagesB.js

--- ImagesA.js
+++
@@ -1,6 +0,0 @@
-
-/**playerimages***/
-
-/*chicagotigers*/
-$('h1.cfm-player-name:contains("Joshua Dobbs")').append
- ('<span class="playerbox"><img src="http://www.daddyleagues.com/img/m18/players/large/8533.png" class="playerimage"></span>');
