558ec7641fb683daacd783344326305eba8953c2
21lions <vicgarza21@gmail.com>
2018-12-09 20:39:23 -0600

    Update and rename maddenA23.js to maddenA24.js

--- maddenA24.js
+++ maddenA24.js
@@ -126,6 +126,101 @@ $("form.form-horizontal").hide();
 });
 
 
+$("#slideshow > div:gt(0)").hide();
+
+var buttons = "<button class=\"slidebtn prev\"><i class=\"fa fa-chevron-circle-left\"></i></button><button class=\"slidebtn next\"><i class=\"fa fa-chevron-circle-right\"></i></button\>";
+
+var slidesl = $('.slideitem').length
+var d = "<li class=\"dot active-dot\">•</li>";
+for (var i = 1; i < slidesl; i++) {
+  d = d+"<li class=\"dot\">•</li>";
+}	
+var dots = "<ul class=\"slider-dots\">" + d + "</ul\>";
+
+$("#slideshow").append(dots).append(buttons);
+var interval = setInterval(slide, 8000);
+
+function intslide(func) {
+	if (func == 'start') { 
+ 	interval = setInterval(slide, 8000);
+	} else {
+		clearInterval(interval);		
+		}
+}
+
+function slide() {
+		sact('next', 0, 1200);
+}
+	
+function sact(a, ix, it) {
+        var currentSlide = $('.current');
+        var nextSlide = currentSlide.next('.slideitem');
+        var prevSlide = currentSlide.prev('.slideitem');
+		    var reqSlide = $('.slideitem').eq(ix);
+
+		    var currentDot = $('.active-dot');
+    	  var nextDot = currentDot.next();
+    	  var prevDot = currentDot.prev();
+		    var reqDot = $('.dot').eq(ix);
+		
+        if (nextSlide.length == 0) {
+      		nextDot = $('.dot').first();
+            nextSlide = $('.slideitem').first();
+            }
+
+        if (prevSlide.length == 0) {
+      		prevDot = $('.dot').last();
+            prevSlide = $('.slideitem').last();
+            }
+			
+		if (a == 'next') {
+			var Slide = nextSlide;
+			var Dot = nextDot;
+			}
+			else if (a == 'prev') {
+				var Slide = prevSlide;
+				var Dot = prevDot;
+				}
+				else {
+					var Slide = reqSlide;
+					var Dot = reqDot;
+					}
+
+        currentSlide.fadeOut(it).removeClass('current');
+        Slide.fadeIn(it).addClass('current');
+		
+    	currentDot.removeClass('active-dot');
+    	Dot.addClass('active-dot');
+}	
+
+$('.next').on('click', function(){
+		intslide('stop');						
+		sact('next', 0, 200);
+		intslide('start');						
+	});//next
+
+$('.prev').on('click', function(){
+		intslide('stop');						
+		sact('prev', 0, -200);
+		intslide('start');						
+	});//prev
+
+$('.dot').on('click', function(){
+		intslide('stop');
+		var index  = $(this).index();
+		sact('dot', index, 400);
+		intslide('start');						
+	});
+
+
+
+ $('div#hp-stories').attr('class', 'slideshow');
+  $('.blogpost.clearfix').attr('class', 'slideitem current');
+   $('.blogpostbody').attr('class', 'info');
+    $('img.blogpostimage').attr('class', 'img');
+      $('  div#hp-stories h4').attr('class', '.carousel-caption h3');
+
+  $("#slideshow > div:gt(0)").hide();
 
 
 /***StarFlash***/
