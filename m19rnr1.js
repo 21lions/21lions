4ab60d6fe006ce6b1ed584d242dfda43676a6b21
21lions <vicgarza21@gmail.com>
2019-01-13 16:03:03 -0600

    Update and rename m19rnr.js to m19rnr1.js

--- m19rnr1.js
+++ m19rnr1.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
 /***menu league title and additional links********************************************************************************************/
-$( "a.navbar-brand" ).text( "Real NFL Rules" );
+$( "a.navbar-brand" ).text( "RNR" );
 $("div#dlmaddenmenu ul.navbar-nav ").append('<a href="http://www.daddyleagues.com/rnr/admin/sync_week" class="btn btn-info ajax" role="button" data-method="post" data-disabled="true" data-loader=".ajax-loader-roster" data-html=".rosterresults">  <img class="ajax-loader-roster" style="display:none" alt="Sending ..." src="/img/ajax-loader.gif"> <em class="icon-wrench"></em></a>');
 
 /***scoreboard**********************/
