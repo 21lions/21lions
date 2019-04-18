3881690cff84c6b420dedacab16a23ae78fd612c
21lions <vicgarza21@gmail.com>
2019-04-18 13:43:41 -0500

    Update and rename m203.js to m204.js

--- m204.js
+++ m204.js
@@ -1,4 +1,3 @@
-$(document).ready(function(){
 
 /***menu league title and additional links********************************************************************************************/
 $( "a.navbar-brand" ).text( "Heavyhitters" );
