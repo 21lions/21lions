44960e0754a5848668ad3e55e92a5dbfaaa41c95
21lions <vicgarza21@gmail.com>
2019-06-21 21:18:55 -0500

    Update and rename esticks.js to esticks1.js

--- esticks1.js
+++ esticks1.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-$('nav.navbar.navbar-expand-lg.dlnavbar.dlnavbg').prepend($('<img>',{id:'theImg',src:'https://i.imgur.com/7LsnxsZ.png'}));
+$('nav.navbar.navbar-expand-lg.dlnavbar.dlnavbg').prepend($('<img>',{id:'theImg',src:'https://i.imgur.com/353tiKZ.png'}));
 
 $('.col-md-9.col-xs-9.col-sm-9').attr('class', 'col-md-12 col-xs-12 col-sm-12 sl');
  $('.col-md-10.col-xs-10.col-sm-10').attr('class', 'col-md-12 col-xs-12 col-sm-12 sl');
