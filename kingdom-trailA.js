03867f83189278a677d865ee867c93b8ce2aad6c
21lions <vicgarza21@gmail.com>
2019-08-12 17:57:37 -0500

    Create kingdom-trailA.js

---
+++ kingdom-trailA.js
@@ -0,0 +1,96 @@
+/*STORYLINES*/
+ $('.blogpost.clearfix').attr('class',  'col-xl-4 col-md-6 col-sm-12 blogpost clearfix');
+
+/*GOW*/
+$('.col-3.d-flex.align-items-center.justify-content-center.rounded-left').attr('class', ' col-4 d-flex align-items-center justify-content-center rounded-left');
+$('.col-3.bg-inverse.d-flex.align-items-center.justify-content-center.rounded-right').attr('class', ' col-4 bg-inverse d-flex align-items-center justify-content-center rounded-right');
+ $('.row.row-flush .col-3:nth-child(2)').attr('class', ' col-2 gow away score');
+$('.row.row-flush .col-3:nth-child(3)').attr('class', '  col-2 gow home score');
+ $('.align-right.clickable.ng-scope').attr('class', ' col-lg-12 col-md-12 col-sm-12 col-xs-12 GOW left');
+ $('.clickable.ng-scope').attr('class',  'col-lg-12 col-md-12 col-sm-12 col-xs-12  GOW right');
+ $('.gameoftheweek .cfm-table tr td.cfm-at').attr('class', 'col-lg-12 col-md-12 col-sm-12 col-xs-12 at');
+$('.gameoftheweek').attr('class', 'col-lg-12 col-md-12 col-sm-12 col-xs-12 gameoftheweek');
+$('.gameoftheweek .GOW').wrapInner('<div class="record"></div>');
+ $( ".gow.away.score" ).appendTo( $( ".col-6.d-flex.align-items-center.justify-content-center.rounded-left" ) );
+$( ".gow.home.score" ).appendTo( $( ".col-6.bg-inverse.d-flex.align-items-center.justify-content-center.rounded-right" ) );
+
+//HOME-CHI
+$('.col-4.bg-inverse.d-flex.align-items-center.justify-content-center.rounded-right[style*="/img/nfl/teams/right/0.png"]').css( 'background-image', 'url("/img/nfl/teams/left/0.png")');
+//HOME-CIN
+$('.col-4.bg-inverse.d-flex.align-items-center.justify-content-center.rounded-right[style*="/img/nfl/teams/right/1.png"]').css( 'background-image', 'url("/img/nfl/teams/left/1.png")');
+//HOME-BUF
+$('.col-4.bg-inverse.d-flex.align-items-center.justify-content-center.rounded-right[style*="/img/nfl/teams/right/2.png"]').css( 'background-image', 'url("/img/nfl/teams/left/2.png")');
+//HOME-DEN
+$('.col-4.bg-inverse.d-flex.align-items-center.justify-content-center.rounded-right[style*="/img/nfl/teams/right/3.png"]').css( 'background-image', 'url("/img/nfl/teams/left/3.png")');
+//HOME-CLE
+$('.col-4.bg-inverse.d-flex.align-items-center.justify-content-center.rounded-right[style*="/img/nfl/teams/right/4.png"]').css( 'background-image', 'url("/img/nfl/teams/left/4.png")');
+//HOME-TB
+$('.col-4.bg-inverse.d-flex.align-items-center.justify-content-center.rounded-right[style*="/img/nfl/teams/right/5.png"]').css( 'background-image', 'url("/img/nfl/teams/left/5.png")');
+//HOME-ARI
+$('.col-4.bg-inverse.d-flex.align-items-center.justify-content-center.rounded-right[style*="/img/nfl/teams/right/6.png"]').css( 'background-image', 'url("/img/nfl/teams/left/6.png")');
+//HOME-LAC
+$('.col-4.bg-inverse.d-flex.align-items-center.justify-content-center.rounded-right[style*="/img/nfl/teams/right/7.png"]').css( 'background-image', 'url("/img/nfl/teams/left/7.png")');
+//HOME-KC
+$('.col-4.bg-inverse.d-flex.align-items-center.justify-content-center.rounded-right[style*="/img/nfl/teams/right/8.png"]').css( 'background-image', 'url("/img/nfl/teams/left/8.png")');
+//HOME-IND
+$('.col-4.bg-inverse.d-flex.align-items-center.justify-content-center.rounded-right[style*="/img/nfl/teams/right/9.png"]').css( 'background-image', 'url("/img/nfl/teams/left/9.png")');
+//HOME-DAL
+$('.col-4.bg-inverse.d-flex.align-items-center.justify-content-center.rounded-right[style*="/img/nfl/teams/right/10.png"]').css( 'background-image', 'url("/img/nfl/teams/left/10.png")');
+//HOME-MIA
+$('.col-4.bg-inverse.d-flex.align-items-center.justify-content-center.rounded-right[style*="/img/nfl/teams/right/11.png"]').css( 'background-image', 'url("/img/nfl/teams/left/11.png")');
+//HOME-PHI
+$('.col-4.bg-inverse.d-flex.align-items-center.justify-content-center.rounded-right[style*="/img/nfl/teams/right/12.png"]').css( 'background-image', 'url("/img/nfl/teams/left/12.png")');
+//HOME-ATL
+$('.col-4.bg-inverse.d-flex.align-items-center.justify-content-center.rounded-right[style*="/img/nfl/teams/right/13.png"]').css( 'background-image', 'url("/img/nfl/teams/left/13.png")');
+//HOME-SF
+$('.col-4.bg-inverse.d-flex.align-items-center.justify-content-center.rounded-right[style*="/img/nfl/teams/right/14.png"]').css( 'background-image', 'url("/img/nfl/teams/left/14.png")');
+//HOME-NYG
+$('.col-4.bg-inverse.d-flex.align-items-center.justify-content-center.rounded-right[style*="/img/nfl/teams/right/15.png"]').css( 'background-image', 'url("/img/nfl/teams/left/15.png")');
+//HOME-JAC
+$('.col-4.bg-inverse.d-flex.align-items-center.justify-content-center.rounded-right[style*="/img/nfl/teams/right/16.png"]').css( 'background-image', 'url("/img/nfl/teams/left/16.png")');
+//HOME-NYJ
+$('.col-4.bg-inverse.d-flex.align-items-center.justify-content-center.rounded-right[style*="/img/nfl/teams/right/17.png"]').css( 'background-image', 'url("/img/nfl/teams/left/17.png")');
+//HOME-DET
+$('.col-4.bg-inverse.d-flex.align-items-center.justify-content-center.rounded-right[style*="/img/nfl/teams/right/18.png"]').css( 'background-image', 'url("/img/nfl/teams/left/18.png")');
+//HOME-GB
+$('.col-4.bg-inverse.d-flex.align-items-center.justify-content-center.rounded-right[style*="/img/nfl/teams/right/19.png"]').css( 'background-image', 'url("/img/nfl/teams/left/19.png")');
+//HOME-CAR
+$('.col-4.bg-inverse.d-flex.align-items-center.justify-content-center.rounded-right[style*="/img/nfl/teams/right/20.png"]').css( 'background-image', 'url("/img/nfl/teams/left/20.png")');
+//HOME-NE
+$('.col-4.bg-inverse.d-flex.align-items-center.justify-content-center.rounded-right[style*="/img/nfl/teams/right/21.png"]').css( 'background-image', 'url("/img/nfl/teams/left/21.png")');
+//HOME-OAK
+$('.col-4.bg-inverse.d-flex.align-items-center.justify-content-center.rounded-right[style*="/img/nfl/teams/right/22.png"]').css( 'background-image', 'url("/img/nfl/teams/left/22.png")');
+//HOME-LAR
+$('.col-4.bg-inverse.d-flex.align-items-center.justify-content-center.rounded-right[style*="/img/nfl/teams/right/23.png"]').css( 'background-image', 'url("/img/nfl/teams/left/23.png")');
+//HOME-BAL
+$('.col-4.bg-inverse.d-flex.align-items-center.justify-content-center.rounded-right[style*="/img/nfl/teams/right/24.png"]').css( 'background-image', 'url("/img/nfl/teams/left/24.png")');
+//HOME-WAS
+$('.col-4.bg-inverse.d-flex.align-items-center.justify-content-center.rounded-right[style*="/img/nfl/teams/right/25.png"]').css( 'background-image', 'url("/img/nfl/teams/left/25.png")');
+//HOME-NO
+$('.col-4.bg-inverse.d-flex.align-items-center.justify-content-center.rounded-right[style*="/img/nfl/teams/right/26.png"]').css( 'background-image', 'url("/img/nfl/teams/left/26.png")');
+//HOME-SEA
+$('.col-4.bg-inverse.d-flex.align-items-center.justify-content-center.rounded-right[style*="/img/nfl/teams/right/27.png"]').css( 'background-image', 'url("/img/nfl/teams/left/27.png")');
+//HOME-PIT
+$('.col-4.bg-inverse.d-flex.align-items-center.justify-content-center.rounded-right[style*="/img/nfl/teams/right/28.png"]').css( 'background-image', 'url("/img/nfl/teams/left/28.png")');
+//HOME-TEN
+$('.col-4.bg-inverse.d-flex.align-items-center.justify-content-center.rounded-right[style*="/img/nfl/teams/right/29.png"]').css( 'background-image', 'url("/img/nfl/teams/left/29.png")');
+//HOME-MIN
+$('.col-4.bg-inverse.d-flex.align-items-center.justify-content-center.rounded-right[style*="/img/nfl/teams/right/30.png"]').css( 'background-image', 'url("/img/nfl/teams/left/30.png")');
+//HOME-HOU
+$('.col-4.bg-inverse.d-flex.align-items-center.justify-content-center.rounded-right[style*="/img/nfl/teams/right/31.png"]').css( 'background-image', 'url("/img/nfl/teams/left/31.png")');
+
+
+
+/*Playoff Race****/
+  $("div#afc_playoff .list-group-item.cfm-hp-playoffrace:nth-child(n+8)").hide();
+    $("div#afc_playoff .list-group h4").on("click", function(){
+        $("div#afc_playoff .list-group-item.cfm-hp-playoffrace:nth-child(n+8)").slideToggle('slow');
+});
+ $("div#nfc_playoff .list-group-item.cfm-hp-playoffrace:nth-child(n+8)").hide();
+    $("div#nfc_playoff .list-group h4").on("click", function(){
+        $("div#nfc_playoff .list-group-item.cfm-hp-playoffrace:nth-child(n+8)").slideToggle('slow');
+});
+
+
+ $('div#afc_playoff h4, div#nfc_playoff h4').attr('class',  'btn btn-primary div#afc_playoff h4 ');
+
